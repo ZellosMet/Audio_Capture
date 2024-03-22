1069e2b496209738b076e750002b7e7c3ae6a6db
ZellosMet <zellossmet@gmail.com>
2024-03-22 19:50:35 +0900

    1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,7 +31,6 @@
 			this.components = new System.ComponentModel.Container();
 			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
 			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-			this.cb_devices = new System.Windows.Forms.ComboBox();
 			this.b_Start = new System.Windows.Forms.Button();
 			this.b_Stop = new System.Windows.Forms.Button();
 			this.cht_Wave = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -39,18 +38,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.cht_Wave)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// cb_devices
-			// 
-			this.cb_devices.FormattingEnabled = true;
-			this.cb_devices.Location = new System.Drawing.Point(13, 13);
-			this.cb_devices.Name = "cb_devices";
-			this.cb_devices.Size = new System.Drawing.Size(775, 24);
-			this.cb_devices.TabIndex = 0;
-			this.cb_devices.SelectedIndexChanged += new System.EventHandler(this.cb_devices_SelectedIndexChanged);
-			// 
 			// b_Start
 			// 
-			this.b_Start.Location = new System.Drawing.Point(13, 93);
+			this.b_Start.Location = new System.Drawing.Point(13, 12);
 			this.b_Start.Name = "b_Start";
 			this.b_Start.Size = new System.Drawing.Size(75, 23);
 			this.b_Start.TabIndex = 1;
@@ -60,7 +50,7 @@
 			// 
 			// b_Stop
 			// 
-			this.b_Stop.Location = new System.Drawing.Point(713, 92);
+			this.b_Stop.Location = new System.Drawing.Point(713, 12);
 			this.b_Stop.Name = "b_Stop";
 			this.b_Stop.Size = new System.Drawing.Size(75, 23);
 			this.b_Stop.TabIndex = 2;
@@ -73,17 +63,15 @@
 			this.cht_Wave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			chartArea1.AxisY.Maximum = 2000D;
-			chartArea1.AxisY.Minimum = -2000D;
 			chartArea1.Name = "ChartArea1";
 			this.cht_Wave.ChartAreas.Add(chartArea1);
-			this.cht_Wave.Location = new System.Drawing.Point(13, 138);
+			this.cht_Wave.Location = new System.Drawing.Point(12, 41);
 			this.cht_Wave.Name = "cht_Wave";
 			series1.ChartArea = "ChartArea1";
 			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
 			series1.Name = "Series1";
 			this.cht_Wave.Series.Add(series1);
-			this.cht_Wave.Size = new System.Drawing.Size(775, 300);
+			this.cht_Wave.Size = new System.Drawing.Size(775, 376);
 			this.cht_Wave.TabIndex = 3;
 			this.cht_Wave.Text = "chart1";
 			// 
@@ -96,11 +84,10 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 429);
 			this.Controls.Add(this.cht_Wave);
 			this.Controls.Add(this.b_Stop);
 			this.Controls.Add(this.b_Start);
-			this.Controls.Add(this.cb_devices);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			((System.ComponentModel.ISupportInitialize)(this.cht_Wave)).EndInit();
@@ -109,8 +96,6 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.ComboBox cb_devices;
 		private System.Windows.Forms.Button b_Start;
 		private System.Windows.Forms.Button b_Stop;
 		private System.Windows.Forms.DataVisualization.Charting.Chart cht_Wave;
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using NAudio.CoreAudioApi;
-using NAudio.Midi;
 using NAudio.Wave;
 using System;
 using System.Collections.Generic;
@@ -11,85 +10,39 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using NAudio.CoreAudioApi;
-using NAudio.Wave;
 
 namespace WindowsFormsApp1
 {
 	public partial class Form1 : Form
 	{
-		WasapiLoopbackCapture capture;
-		WaveIn wi;
 		short[] data;
 		bool check = false;
-
-		private BufferedWaveProvider buffer;
-		private WaveOut waveOut;
 		private WaveIn sourceStream = null;
-
-		private bool listen = false;
 
 		public Form1()
 		{
 			InitializeComponent();
-			MMDeviceEnumerator mde = new MMDeviceEnumerator();
-			MMDeviceCollection devices = mde.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
-			cb_devices.Items.AddRange(devices.ToArray());
-
-			//capture = new WasapiLoopbackCapture();
-			//capture.DataAvailable += Capture_DataAvailable;
-			//wi.DataAvailable += InputBufferToFileCallback;
-
-			//listen = !listen;
-			//if (listen)
-			//	listenBtn.Text = "Stop listening";
-			//else
-			//{
-			//	listenBtn.Text = "Listen";
-			//	sourceStream.StopRecording();
-			//	return;
-			//}
 
 			sourceStream = new WaveIn();
 			sourceStream.WaveFormat = new WaveFormat(44100, 16, 1);
 
-			waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
-
-			sourceStream.DataAvailable += new EventHandler<WaveInEventArgs>(sourceStream_DataAvailable);
-			//sourceStream.RecordingStopped += SourceStream_RecordingStopped; //new EventHandler<StoppedEventArgs>(sourceStream_RecordingStopped);
-
-			buffer = new BufferedWaveProvider(sourceStream.WaveFormat);
-			buffer.DiscardOnBufferOverflow = true;
-			waveOut.DesiredLatency = 51;
-			waveOut.Volume = 1f;
-			waveOut.Init(buffer);
-			
+			sourceStream.DataAvailable += SourceStream_DataAvailable;
+			sourceStream.RecordingStopped += SourceStream_RecordingStopped;			
 		}
 
-		//private void SourceStream_RecordingStopped(object sender, StoppedEventArgs e)
-		//{
-		//	throw new NotImplementedException();
-		//}
-
-		//private void Capture_DataAvailable(object sender, WaveInEventArgs e)
-		private void sourceStream_DataAvailable(object sender, WaveInEventArgs e)
+		private void SourceStream_RecordingStopped(object sender, StoppedEventArgs e)
 		{
-			data = new short[e.BytesRecorded];
-			for (int i = 0, j = 0; i < e.BytesRecorded - 1; i += 2, j++)
-				data[j] = (short)(e.Buffer[i] <<8 | (e.Buffer[i+1]));
+			sourceStream.StopRecording();
+			sourceStream.Dispose();
+		}
+
+		private void SourceStream_DataAvailable(object sender, WaveInEventArgs e)
+		{
+			data = new short[e.Buffer.Length/ 4];
+			for (int i = 0, j = 0; i < e.Buffer.Length / 2-1; i += 2, j++)
+				data[j] = (short)(e.Buffer[i] << 8 | (e.Buffer[i + 1]));
 			check = true;
 		}
-		private void sourceStream_RecordingStopped(object sender, WaveInEventArgs e)
-		{
-			sourceStream.Dispose();
-			waveOut.Dispose();
-		}
-
-		//private void wie_DataAvailable(object sender, WaveEventArgs e)
-		//{
-		//	for (int i = 0, j = 0; i < e.Buffer.Length-1; i += 2, j++)
-		//		data[j] = (short)(e.Buffer[i] << 8 | (e.Buffer[i + 1]));
-		//}
 
 		private void t_Refresh_Wave_Tick(object sender, EventArgs e)
 		{
@@ -105,35 +58,21 @@ namespace WindowsFormsApp1
 
 		private void b_Start_Click(object sender, EventArgs e)
 		{
+			sourceStream = new WaveIn();
+			sourceStream.WaveFormat = new WaveFormat(44100, 16, 1);
+			sourceStream.DataAvailable += SourceStream_DataAvailable;
+			sourceStream.RecordingStopped += SourceStream_RecordingStopped;
 			sourceStream.StartRecording();
-			//capture.StartRecording();
-			//wie.StartRecording();
+
 			t_Refresh_Wave.Enabled = true;
 		}
 
 		private void b_Stop_Click(object sender, EventArgs e)
 		{
 			sourceStream.Dispose();
-			waveOut.Dispose();
-			//capture.StopRecording();
-			//capture.Dispose();
-			//wie.StopRecording();
-			//wie.Dispose();
 			t_Refresh_Wave.Enabled = false;
 			check = false;
 			cht_Wave.Series[0].Points.Clear();
-		}
-
-		private void cb_devices_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			//wie = new NAudio.Wave.WaveOutEvent()
-			//{
-			//	DeviceNumber = cb_devices.SelectedIndex,
-			//	WaveFormat = new NAudio.Wave.WaveFormat(44100, 16, 1),
-			//	BufferMilliseconds = 20
-			//};
-			//wie.
-			//wie.DataAvailable += wie_DataAvailable;
 		}
 	}
 }
